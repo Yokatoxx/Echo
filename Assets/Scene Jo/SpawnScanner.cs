ef5412fcfbf2490e60198b22a442d893033ae787
Yokatoxx <yokatoxx@gmail.com>
2025-02-03 12:00:10 +0100

    Outline

--- Assets/Scene Jo/SpawnScanner.cs
+++ Assets/Scene Jo/SpawnScanner.cs
@@ -7,26 +7,33 @@ public class SpawnScanner : MonoBehaviour
     public GameObject TerrainScannerPrefab;
     public float duration = 10f;
     public float size = 500f;
+    public int numberOfScanners = 1;
+    public float spawnDelay = 1f;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SpawnTerrainScanner();
+            StartCoroutine(SpawnTerrainScanner());
         }
     }
 
-    void SpawnTerrainScanner()
+    IEnumerator SpawnTerrainScanner()
     {
-        GameObject terrainScanner = Instantiate(TerrainScannerPrefab, gameObject.transform.position, Quaternion.identity) as GameObject;
-        ParticleSystem terrainScannerPS = terrainScanner.transform.GetChild(0).GetComponent<ParticleSystem>();
-
-        if (terrainScannerPS != null)
+        for (int i = 0; i < numberOfScanners; i++)
         {
-            var main = terrainScannerPS.main;
-            main.startLifetime = duration;
-            main.startSize = size;
-        }
+            GameObject terrainScanner = Instantiate(TerrainScannerPrefab, transform.position, Quaternion.identity);
+            ParticleSystem terrainScannerPS = terrainScanner.transform.GetChild(0).GetComponent<ParticleSystem>();
 
-        Destroy(terrainScanner, duration+1);
+            if (terrainScannerPS != null)
+            {
+                var main = terrainScannerPS.main;
+                main.startLifetime = duration;
+                main.startSize = size;
+            }
+
+            Destroy(terrainScanner, duration + 1);
+            yield return new WaitForSeconds(spawnDelay);
+        }
     }
 }
